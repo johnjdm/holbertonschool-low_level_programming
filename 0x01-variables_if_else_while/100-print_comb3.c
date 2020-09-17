c6ac75208f514651c67ecadaa637e90a8a3ede4c
johnjdm <johnjdm.ings@live.com>
2020-09-17 19:05:01 +0000

    Add 100-print_comb3.c file 0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ *main - Display all possible different combinations of two digits
+ *Return: 0
+ */
+
+int main(void)
+{
+int x;
+int y = 0;
+
+for (x = 0; x <= 9; x++)
+{
+for (y = x + 1; y <= 9; y++)
+{
+putchar((x % 10) + '0');
+putchar((y % 10) + '0');
+if (!(x == 8 && y == 9))
+putchar(',');
+putchar(' ');
+}
+}
+
+putchar('\n');
+return (0);
+}
