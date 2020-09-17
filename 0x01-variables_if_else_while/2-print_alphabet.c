b742087b4998cb258592bbc454c721d34296e74e
johnjdm <johnjdm.ings@live.com>
2020-09-17 14:02:14 +0000

    Add 2-print_alphabet.c file 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+#include <stdio.h>
+
+/**
+* main - display the alphabet in lowercase, and then in uppercase
+* Return: 0
+*/
+
+int main ()
+{
+char ch;
+
+for (ch = 'A' ; ch <= 'Z' ; ch++) 
+{
+putchar (ch);
+putchar('\n');
+}
+return 0;
+}
