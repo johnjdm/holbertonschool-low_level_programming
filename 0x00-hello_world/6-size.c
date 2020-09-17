d11687cdc14b322637bc4dafc75237572e2897a8
johnjdm <johnjdm.ings@live.com>
2020-09-17 02:36:34 +0000

    6-size.c file 0x00-hello_world

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - Variable size
+* Return: 0
+*/
+
+int main(void)
+{
+printf("Size of a char: %d byte(s)\n", sizeof(char));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long float: %d byte(s)\n", sizeof(float));
+printf("Size of a long long double: %d byte(s)\n", sizeof(double));
+return (0);
+}
