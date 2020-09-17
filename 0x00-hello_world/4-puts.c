077e394604db9c28d110bdc6872a46ae7fa0f753
johnjdm <johnjdm.ings@live.com>
2020-09-17 00:39:40 +0000

    4-puts.c file 0x00-hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 
 /**
- *main - funtion display "Programming is like building a multilingual puzzle.
- */
+ * main - funtion display "Programming is like building a multilingual puzzle.
+ * Return: 0
+*/
 
-int main(void) {
+int main(void)
+{
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
 }
