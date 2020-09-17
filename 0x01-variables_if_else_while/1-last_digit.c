d70f092d64f6cc0e2755784bedce532a67b33aa8
johnjdm <johnjdm.ings@live.com>
2020-09-17 12:18:14 +0000

    Add 1-last_digit.c file 0x01-variables_if_else_while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,3 +6,32 @@
 * main - display the last digit of the number stored in the variable n.
 * Return: 0
 */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+  int n, lastDigit;
+
+  srand(time(0));
+  n = rand() - RAND_MAX / 2;
+
+  /* Find last digit */
+  lastDigit = n % 10;   
+  
+  printf("Last digit of %d is %d ", n, lastDigit);
+
+  if (lastDigit > 5)
+    { 
+      printf("and is greater than 5\n");
+    }
+  if (lastDigit == 0)
+    {
+      printf("and is 0\n");   
+    }
+  else
+    {
+      printf("and is less than 6 and not 0\n");   
+    }
+
+  return (0);
+}
