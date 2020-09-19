688b7558ea664160a8a31f7bf4320b2b75dca89f
johnjdm <johnjdm.ings@live.com>
2020-09-19 10:33:23 +0000

    Add 6-abs.c file 0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* _abs - Write a function that computes the absolute value of an integer.
+* @n: the character to check
+* Return: 0
+*/
+
+int _abs(int n)
+{
+
+if (n < 0)
+{
+n *= -1;
+return (n);
+}
+else
+{
+return (n);
+}
+}
