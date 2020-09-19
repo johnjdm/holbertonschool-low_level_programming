18c93693918adddcf710b1456a920c3b46d2f12f
johnjdm <johnjdm.ings@live.com>
2020-09-19 07:03:52 +0000

    Add 4-isalpha.c file 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
 
 /**
- *_isalpha - display 1 to lower/upper case, 0 otherwise
- *@c: parameter character or ascii code
- *Return: 0 or 1
- */
+* _isalpha - display 1 to lower/upper case, 0 otherwise
+* @c: parameter character or ascii code
+* Return: 0 or 1
+*/
 
 int _isalpha(int c)
 {
 int sol;
 
-if((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 {
-sol=1;
+sol = 1;
 }
-else 
+else
 {
-sol=0;
+sol = 0;
 }
-return sol;
+
+return (sol);
 }
