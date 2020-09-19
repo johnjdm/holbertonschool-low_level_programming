8b497c54515a94fc3443afad81fab7541412156a
johnjdm <johnjdm.ings@live.com>
2020-09-19 19:32:54 +0000

    Add 101-natural.c file 0x03-debugging

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
-* main - Prints the sum of all the multiples of 3 or 5 
-* below 1024 (excluded).
+* main - Prints the sum of all the multiples of 3
+* or 5 below 1024 (excluded).
 * Return: 0
 */
 
