d6966d6007322fceb0f32f76f2b28ee37fe83ad6
johnjdm <johnjdm.ings@live.com>
2020-09-19 02:50:22 +0000

    2-print_alphabet_x10.c file 0x02-funtions_nested_loops

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *print_alphabet_x10 - display alphabet x10
+ *Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+char alp;
+int i;
+
+for(i = 0; i <= 9; i++)
+{
+alp = 'a';
+while (alp <= 'z')
+{
+_putchar(alp);
+alp++;
+}
+_putchar('\n');
+}
+}
