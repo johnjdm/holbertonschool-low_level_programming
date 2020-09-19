40c648184d6f21660a9f0c1d04cf7265f7084b2d
johnjdm <johnjdm.ings@live.com>
2020-09-19 06:12:59 +0000

    Add 1-alphabet.c file 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,18 @@
 #include "holberton.h"
+
 /**
- * print_alphabet - display prints the alphabet, in lowercase, followed by a new line.
- * Return: void
- */
+* print_alphabet - display prints the alphabet, in lowercase, followed by a new line.
+* Return: void
+*/
+
 void print_alphabet(void)
 {
 char alp;
 
 for (alp = 'a'; alp <= 'z'; alp++)
+{
 _putchar(alp);
+}
+
 _putchar('\n');
 }
