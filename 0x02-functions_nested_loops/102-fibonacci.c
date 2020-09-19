72e3bc58db5cd13f77863218b78404ca81eeb307
johnjdm <johnjdm.ings@live.com>
2020-09-19 19:53:29 +0000

    Add 102-fibonacci.c file 0x03-debugging

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+int i, n, t1 = 0, t2 = 1, nextTerm;
+
+for (i = 1; i <= 50; ++i)
+{
+printf("%d, ", t1);
+nextTerm = t1 + t2;
+t1 = t2;
+t2 = nextTerm;
+}
+
+return (0);
+}
