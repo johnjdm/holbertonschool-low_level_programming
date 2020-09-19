dc70af5747a3f625f27c366402f50cb2204ee3da
johnjdm <johnjdm.ings@live.com>
2020-09-19 06:49:52 +0000

    Add 3-islower.c file 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,22 +1,23 @@
 #include "holberton.h"
 
 /**
- *_islower - display 1 is lower char, 0 otherwise
- *@c: The character or ascii code
- *Return: 0 or 1
- */
+* _islower - display 1 is lower char, 0 otherwise
+* @c: The character or ascii code
+* Return: 0 or 1
+*/
 
 int _islower(int c)
 {
 int sol;
 
-if(c >= 97 && c <= 122)
+if (c >= 97 && c <= 122)
 {
-sol=1;
+sol = 1;
 }
 else if (c >= 65 && c <= 90) 
 {
-sol=0;
+sol = 0;
 }
+
 return sol;
 }
