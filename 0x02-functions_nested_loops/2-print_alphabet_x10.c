1def5f3dee703b0fd9730d2109a98deeb9e15faf
johnjdm <johnjdm.ings@live.com>
2020-09-19 06:33:36 +0000

    Add 2-print_alphabet_x10.c file 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 
 /**
- *print_alphabet_x10 - display alphabet x10
- *Return: void
- */
+* print_alphabet_x10 - display alphabet x10
+* Return: void
+*/
 
 void print_alphabet_x10(void)
 {
