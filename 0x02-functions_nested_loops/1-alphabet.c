8efc09c32af5955eb75a9e1fea5a6d6285513df9
johnjdm <johnjdm.ings@live.com>
2020-09-19 05:50:16 +0000

    Add 1-alphabet.c file 0x02-functions_nested_loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,7 +3,7 @@
 /**
  *print_alphabet - display prints the alphabet, in lowercase, followed by a new line.
  *Return: void
- */
+*/
 
 void print_alphabet(void)
 {
@@ -13,5 +13,6 @@ for (alp = 'a'; alp <= 'z'; alp++)
 {
 _putchar(alp);
 }
+
 _putchar('\n');
 }
