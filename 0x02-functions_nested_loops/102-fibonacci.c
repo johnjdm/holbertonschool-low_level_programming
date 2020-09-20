9bcec36d6884e386714287b4fb1278d86880c112
johnjdm <johnjdm.ings@live.com>
2020-09-20 00:12:50 +0000

    Add 102-fibonacci.c file 0x02-funtions_nested_loops

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,12 +14,15 @@ t1 = 1;
 t2 = 2;
 n = 50;
 
-for (i = 1; i <= n; ++i)
+for (i = 1; i < n; ++i)
 {
 printf("%lu, ", t1);
 nextTerm = t1 + t2;
 t1 = t2;
 t2 = nextTerm;
+}
+if (i <= n) {
+printf("%lu", t1);
 }
 
 printf("\n");
