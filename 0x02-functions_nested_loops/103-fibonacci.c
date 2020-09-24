11c82fb24b25cb2e2e32d074c0e39732fbd6224d
johnjdm <johnjdm.ings@live.com>
2020-09-24 15:53:22 +0000

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,17 +7,17 @@
 
 int main(void)
 {
-long t1 = 0, t2 = 1, nextTerm = 0, par = 0;
+	long t1 = 0, t2 = 1, nextTerm = 0, par = 0;
 
-for (nextTerm = 0; nextTerm <= 4000000; ++nextTerm)
-{
-nextTerm = t1 + t2;
-t1 = t2;
-t2 = nextTerm;
+	for (nextTerm = 0; nextTerm <= 4000000; ++nextTerm)
+	{
+		nextTerm = t1 + t2;
+		t1 = t2;
+		t2 = nextTerm;
 
-if ((nextTerm % 2) == 0)
-par = par + nextTerm;
-}
-printf("%ld\n", par);
-return (0);
+		if ((nextTerm % 2) == 0)
+			par = par + nextTerm;
+	}
+	printf("%ld\n", par);
+	return (0);
 }
