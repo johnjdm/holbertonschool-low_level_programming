e819c406b532a16df8b4fdbb3a8eddf7e4d67546
johnjdm <johnjdm.ings@live.com>
2020-09-25 02:45:51 +0000

    Add 4-print_most_numbers.c file 0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,16 +7,16 @@
 
 void print_most_numbers(void)
 {
-       	int i = 0;
+	int i = 0;
 
-       	while (i <= 9)
-       	{
-       		if (i != 2 && i != 4)
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
 		{
 			_putchar(i + '0');
 		}
-       		i++;
-       	}
+		i++;
+	}
 
-      	_putchar('\n');
+	_putchar('\n');
 }
