36efee7fd02373867b02ed3c6f61d9fd6dbcec7f
johnjdm <johnjdm.ings@live.com>
2020-09-25 03:56:18 +0000

    Add #include holberton.h file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ *print_diagonal - print '\' diagonal
+ *@n: parameter integer
+ *Return: void
+ */
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = 0; j < i; u++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
