297b15ba9393639beae7b9d12312b1979ba1dfa0
johnjdm <johnjdm.ings@live.com>
2020-09-25 03:45:38 +0000

    Add 6-print_line.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+* print_line - print '_' n times
+* @n: parameter enteger
+* Return: int n
+*/
+
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		for (i = 0; i >= n; i++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
