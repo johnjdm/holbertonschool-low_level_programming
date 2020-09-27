4a187ca70a87ad19ca5835156d9e6a2af518a86d
johnjdm <johnjdm.ings@live.com>
2020-09-27 22:13:14 +0000

    Recursive form 101-print_number.c file 0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prime factor
+ * Description: prime factor of the number 612852475143
+ * Return: 0
+ */
+int main(void)
+{
+	long i, num;
+
+	num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+
+	printf("%lu\n", i);
+	return (0);
+}
