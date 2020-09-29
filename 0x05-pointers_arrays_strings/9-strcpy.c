252ce706d7e7b48d21bfe2bba08912df890ca3d8
johnjdm <johnjdm.ings@live.com>
2020-09-29 23:43:23 +0000

    Add 9-strcpy.c file 0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+* _strcpy - Write a function that copies the string pointed to by src
+* @dest: pointer char void
+* @src: pointer char source
+* Return: dest data
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i, size_t;
+	size_t = 0;
+
+	while (src[size_t])
+	{
+		size_t++;
+	}
+
+	for (i = 0; i < size_t && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < size_t; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
