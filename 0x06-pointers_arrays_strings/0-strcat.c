202be05dde78e59d14e96deeee5af9e957839a99
johnjdm <johnjdm.ings@live.com>
2020-10-01 11:41:21 +0000

    Add 0-strcat.c file 0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcat - concat strings
+ * @dest: ouputstr
+ * @src: inputstr
+ * Return: pointer to char
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i = 0, j = 0;
+
+	for (; dest[i] != '\0'; i++)
+		;
+	for (; src[j] != '\0'; j++)
+		dest[j+i] = src[j];
+	
+	return (dest);
+}
