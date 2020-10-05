6e5a008c484133668c946fa3732c8de0796eaf9b
johnjdm <johnjdm.ings@live.com>
2020-10-05 14:59:57 +0000

    Add 2-strchr.c file 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,12 @@
 #include "holberton.h"
-
+#include <stdio.h>
 /**
  * _strspn - gets the length of a prefix substring.
- * @s: pointer parameter
- * @c: pointer parameter, value accept
- * Return: returns first occurence of c in s, or NULL if not found
+ * @s: pointer parameter.
+ * @c: pointer parameter, value accept.
+ * Return: returns first occurence of c in s, or NULL if not found.
  */
+
 char *_strchr(char *s, char c)
 {
 	unsigned int i;
