5b86b87c05872fbe2e7534e4227500f338d20660
johnjdm <johnjdm.ings@live.com>
2020-10-05 13:56:05 +0000

    Add 2-strchr.c file 0x07-pointers_arrays_strings

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,11 +13,12 @@ char *_strchr(char *s, char c)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
+		if(s[i] == c)
 		{
-			return (s + i);
+			s = &s[i];
+			break;
 		}
 	}
 
-	return (0);
+	return (s);
 }
