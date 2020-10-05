d7406899d8af2f5e27f958032891c1b3e718619d
johnjdm <johnjdm.ings@live.com>
2020-10-05 19:44:34 +0000

    Add 7-print_chessboard.c file 0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to the matrix
+ * Description: Function that prints the chessboard.
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(i + a) + j));
+		}
+		_putchar('\n');
+	}
+}
